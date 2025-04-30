8a70390448f586174535864077b9c0ac7e6db8c2
chiancw <chiancw@gmail.com>
2025-04-30 19:36:59 +0800

    first draft

---
+++ main.tf
@@ -0,0 +1,16 @@
+module "lz_vending" {
+  source  = "Azure/lz-vending/azurerm"
+  version = "v5.1.2" # change this to your desired version, https://www.terraform.io/language/expressions/version-constraints
+
+  # Set the default location for resources
+  location = "southeastasia"
+
+  # subscription variables
+  subscription_alias_enabled = true
+  subscription_billing_scope = "/providers/Microsoft.Billing/billingAccounts/7c0b9089-fd64-5d61-e847-2065f602106d:ff81f6a8-4163-4aa4-95b7-b3b34853b813_2019-05-31/billingProfiles/YOLD-AHRU-BG7-PGB/invoiceSections/PILE-J4JK-PJA-PGB"
+  subscription_display_name  = "bdotest1sub"
+  subscription_alias_name    = "bdotest1subalias"
+  subscription_workload      = "Production"
+
+}
+
---
+++ providers.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_version = "~> 1.10"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+
+
+  ## below block defines the backend that contains tfstate for this deployment
+  backend "azurerm" {
+    subscription_id      = "ad6f62ba-74ae-4f03-8123-5431c364621d" 
+    resource_group_name  = "bdosubvendrg"
+    storage_account_name = "bdosubsa123"
+    container_name       = "tfstate"
+    key                  = "bdosubvend.tfstate"
+  }
+
+
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+#   use_oidc = true
+  subscription_id = "ad6f62ba-74ae-4f03-8123-5431c364621d"
+
+}
