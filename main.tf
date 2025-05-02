6866bf1528a1ea46510783710e1a7ab918c0862c
chiancw <chiancw@gmail.com>
2025-05-02 14:57:17 +0800

    add subscription 6

--- main.tf
+++ main.tf
@@ -29,3 +29,19 @@ module "lz_vending1" {
   subscription_workload      = "Production"
 
 }
+
+module "lz_vending2" {
+  source  = "Azure/lz-vending/azurerm"
+  version = "v5.1.2" # change this to your desired version, https://www.terraform.io/language/expressions/version-constraints
+
+  # Set the default location for resources
+  location = "southeastasia"
+
+  # subscription variables
+  subscription_alias_enabled = true
+  subscription_billing_scope = "/providers/Microsoft.Billing/billingAccounts/7c0b9089-fd64-5d61-e847-2065f602106d:ff81f6a8-4163-4aa4-95b7-b3b34853b813_2019-05-31/billingProfiles/YOLD-AHRU-BG7-PGB/invoiceSections/PILE-J4JK-PJA-PGB"
+  subscription_display_name  = "bdotest6sub"
+  subscription_alias_name    = "bdotest6subalias"
+  subscription_workload      = "Production"
+
+}
